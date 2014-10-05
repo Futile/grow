81259f4fa312d292d6f0b5cf8d939a9bee51674c
Felix Rath <felix.rath@rwth-aachen.de>
2014-10-05 02:55:38 +0200

    added sdl2-dependency and simple window code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+extern crate sdl2;
+extern crate native;
+
+use sdl2::video::{Window, PosCentered, OpenGL};
+use sdl2::event::{QuitEvent, NoEvent, poll_event};
+use sdl2::timer::{delay};
+
 fn main() {
-    println!("Hello, world!")
+    sdl2::init(sdl2::InitEverything);
+
+    let window = match Window::new("yolo", PosCentered, PosCentered, 640, 480, OpenGL) {
+        Ok(window) => window,
+        Err(err) => fail!("failed to create window: {}", err)
+    };
+
+    window.show();
+
+    'event : loop {
+        match poll_event() {
+            QuitEvent(_) => break 'event,
+            NoEvent => continue,
+            event => println!("event: {}", event),
+        }
+    }
+
+    sdl2::quit();
 }
